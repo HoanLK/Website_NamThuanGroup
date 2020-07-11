9b956fbdf084a849cc6149f7b2c3cf26a627da0f
Hoan LK <hoanlk93@gmail.com>
2020-07-11 14:58:41 +0700

    Update

---
+++ CMS/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Controllers
+{
+    [RoutePrefix("about-us")]
+    public class AboutController : Controller
+    {
+        // GET: /about-us
+        [Route()]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
